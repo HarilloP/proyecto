5b4041279bc9e2fc02f10d65d9d79706298a3f41
Victor Manuel <victormanuelhpyuma@gmail.com>
2024-09-20 13:58:49 +0200

    Prettier

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,14 +8,13 @@ const app = express();
 
 const { PORT } = process.env;
 // login
-app.get("/login", function (req, res) {
-    res.send("login");
+app.get('/login', function (req, res) {
+    res.send('login');
 });
 // algo
-app.get("/", function (req, res) {
-    res.send("Hello world");
+app.get('/', function (req, res) {
+    res.send('Hello world');
 });
-
 
 app.listen(PORT, () => {
     console.log(`http://127.0.0.1:${PORT}`);
